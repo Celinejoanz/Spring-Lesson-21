47d9cd3d085903c1cbbe8bc572e65a5d70fb5cca
Chidimma Uba <chidimmajuba@gmail.com>
2018-07-17 15:02:50 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Spring-Lesson-21" 
---
+++ src/main/java/com/example/springlesson_21/Springlesson21Application.java
@@ -0,0 +1,12 @@
+package com.example.springlesson_21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springlesson21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springlesson21Application.class, args);
+	}
+}
